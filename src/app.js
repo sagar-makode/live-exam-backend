dc9988b77d03af5cd15461d3dd3795c63bd2b881
sagar-makode <makodepatil1@gmail.com>
2024-04-20 17:56:20 +0530

    remove cors

--- src/app.js
+++ src/app.js
@@ -12,11 +12,7 @@ const connectDB = require("./db/connection")
 //Port
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: ["http://localhost:3000"],
-  methods: ["GET", "POST","PUT","DELETE"],
-  credentials: true
-}));
+app.use(cors());
 
 // Middleware
 app.use(cookieParser());
